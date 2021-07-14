8cc2f139ad1a0da4105a3afe39d73a72f3dcec1d
jdkfx <amateurranch@gmail.com>
2021-07-14 20:14:40 +0900

    :sparkles: バッテリー状態の確認

--- temperature-checker.ino
+++ temperature-checker.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <M5StickC.h>
+#define checkResetOn()  if(M5.Axp.GetBtnPress()==2){esp_restart();}
 
+void setup() {
+  // 文字列の表示
+  M5.begin();
+  M5.Lcd.print("Hello, world.");
+
+  // ディスプレイの表示方法
+  M5.Axp.ScreenBreath(9);
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setTextColor(YELLOW,BLUE);
+  M5.Lcd.setTextFont(2);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setCursor(0,0);
+  M5.Lcd.printf("Hello, world.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // バッテリー状態の確認
+  float v,a,t;
+  v = M5.Axp.GetBatVoltage();   // 電圧
+  a = M5.Axp.GetBatCurrent();   // 電流
+  t = M5.Axp.GetTempInAXP192(); // 温度
+  M5.Lcd.setCursor(0,64);
+  M5.Lcd.printf("Bat %3.1fV %5.1fmA %4.1fC ",v,a,t);
 
+  // 電源のリセット
+  checkResetOn();
+  delay(10);
 }
